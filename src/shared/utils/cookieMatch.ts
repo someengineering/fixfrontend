35b9804f4a2e016202631135a54b44737767b020
Sina Javaheri <sinajavaheri@gmail.com>
2023-09-21 14:18:57 +0200

    fix(cookie-match): add null check when there's no cookie

--- src/shared/utils/cookieMatch.ts
+++ src/shared/utils/cookieMatch.ts
@@ -1,6 +1,6 @@
 export const cookieMatch = (name: string, match?: string | RegExp | ((value: string) => boolean)) => {
-  const splittedByName = document.cookie.split(name + '=')
-  const value = splittedByName[1].split(';')[0]
+  const splittedByName = document.cookie?.split(name + '=')
+  const value = splittedByName?.[1]?.split(';')?.[0]
   if (typeof match === 'function') {
     return match(value)
   } else if (typeof match === 'string') {
