e105263a11cd9174d418d270e05cf5584979e694
Sina Javaheri <sinajavaheri@gmail.com>
2023-10-04 19:10:24 +0200

    fix(external-id): add a button to show/hide the external id

--- src/pages/panel/panel-setup-cloud/ExternalId.tsx
+++ src/pages/panel/panel-setup-cloud/ExternalId.tsx
@@ -1,10 +1,14 @@
 import { Trans } from '@lingui/macro'
-import CopyAll from '@mui/icons-material/CopyAll'
-import { Box, Button, Typography } from '@mui/material'
+import CopyAllIcon from '@mui/icons-material/CopyAll'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+import { Box, Button, IconButton, Typography } from '@mui/material'
 import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
 import { getExternalIdQuery, getOrganizationQuery } from 'src/pages/panel/shared-queries'
 
 export const ExternalId = () => {
+  const [showExternalId, setShowExternalId] = useState(false)
   const { data: organizationData } = useQuery(['organization'], getOrganizationQuery)
   const { data: ExternalIdData } = useQuery(['organization-external-id', organizationData?.[0].id], getExternalIdQuery, {
     enabled: !!organizationData?.[0].id,
@@ -14,13 +18,25 @@ export const ExternalId = () => {
       navigator.clipboard.writeText(ExternalIdData || '')
     } catch {}
   }
+  const handleToggleShowExternalId = () => {
+    setShowExternalId((prev) => !prev)
+  }
   return (
     <>
-      <Typography variant="h6" ml={2} p={2} bgcolor="grey.300">
-        {ExternalIdData}
-      </Typography>
+      <Box display="inline-flex" ml={2} bgcolor="grey.300" alignItems="center">
+        <Typography
+          variant="h6"
+          fontFamily="Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New"
+          p={2}
+        >
+          {showExternalId ? ExternalIdData : '********-****-****-****-************'}
+        </Typography>
+        <Box>
+          <IconButton onClick={handleToggleShowExternalId}>{showExternalId ? <VisibilityIcon /> : <VisibilityOffIcon />}</IconButton>
+        </Box>
+      </Box>
       <Box ml={2}>
-        <Button variant="contained" startIcon={<CopyAll />} onClick={handleCopy}>
+        <Button variant="contained" startIcon={<CopyAllIcon />} onClick={handleCopy}>
           <Trans>Copy</Trans>
         </Button>
       </Box>
