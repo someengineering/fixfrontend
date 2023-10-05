412e3d27c96e78778692759f5f2a388d2a6d0fed
Sina Javaheri <sinajavaheri@gmail.com>
2023-10-05 15:11:33 +0200

    fix(panel-setup-cloud-page): replace the text

--- src/pages/panel/panel-setup-cloud/PanelSetupCloudPage.tsx
+++ src/pages/panel/panel-setup-cloud/PanelSetupCloudPage.tsx
@@ -1,7 +1,6 @@
 import { Trans } from '@lingui/macro'
-import { Box, Button, List, ListItem, Skeleton, Typography } from '@mui/material'
-import { loremIpsum } from 'lorem-ipsum'
-import { Suspense, useMemo } from 'react'
+import { Box, Button, Divider, Skeleton, Typography } from '@mui/material'
+import { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { ErrorBoundaryFallback } from 'src/shared/error-boundary-fallback'
 import { ExternalId } from './ExternalId'
@@ -9,23 +8,50 @@ import { SetupCloudButton } from './SetupCloudButton'
 import { TenantId } from './TenantId'
 
 export default function PanelSetupCloudPage() {
-  const { instructions, mainPragraph } = useMemo(
-    () => ({
-      mainPragraph: loremIpsum({ count: 10 }),
-      instructions: Array.from(Array(5)).map(() => loremIpsum({ count: 2 })),
-    }),
-    [],
-  )
   return (
     <>
       <Typography variant="h1" color="secondary" mb={2}>
         <Trans>Setup cloud</Trans>
       </Typography>
-      <Typography variant="body1" textAlign="justify">
-        {mainPragraph}
+      <Typography variant="h6">
+        <Trans>In the next step we are going to set up the trust between FIX and your AWS cloud account.</Trans>
+      </Typography>
+      <Typography variant="h6" color="warning.main" mt={1}>
+        <Trans>Make sure that you are already logged into the correct AWS account, before pressing the SETUP button.</Trans>
+      </Typography>
+      <Box py={3}>
+        <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
+          <Suspense
+            fallback={
+              <Skeleton variant="rounded">
+                <Button>
+                  <Trans>Go To Setup</Trans>
+                </Button>
+              </Skeleton>
+            }
+          >
+            <SetupCloudButton />
+          </Suspense>
+        </ErrorBoundary>
+      </Box>
+      <Typography variant="body1" my={1}>
+        <Trans>
+          This will deploy a CloudFormation stack that creates a new IAM role in your AWS account. This role will be used by FIX to access
+          your AWS account.
+        </Trans>
+      </Typography>
+      <Divider />
+      <Typography variant="subtitle2" mt={1}>
+        <Trans>Alternatively if you would like to deploy the stack manually, you can use the following CloudFormation template</Trans>:
+        <Button href="https://fix-aws-integration.s3.amazonaws.com/fix-aws-integration.yaml" target="_blank" variant="text">
+          https://fix-aws-integration.s3.amazonaws.com/fix-aws-integration.yaml
+        </Button>
+      </Typography>
+      <Typography variant="body1" mt={1}>
+        <Trans>The Stack requires the following parameters</Trans>:
       </Typography>
       <Box display="flex" py={2} alignItems="center">
-        <Typography variant="h5">
+        <Typography variant="body1">
           <Trans>External Id</Trans>:
         </Typography>
         <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
@@ -41,44 +67,10 @@ export default function PanelSetupCloudPage() {
         </ErrorBoundary>
       </Box>
       <Box display="flex" py={2} alignItems="center">
-        <Typography variant="h5">
+        <Typography variant="body1">
           <Trans>Tenant Id</Trans>:
         </Typography>
-        <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
-          <Suspense
-            fallback={
-              <Box ml={2}>
-                <Skeleton variant="rectangular" width={376} height={52} />
-              </Box>
-            }
-          >
-            <TenantId />
-          </Suspense>
-        </ErrorBoundary>
-      </Box>
-      <List>
-        {instructions.map((text, i) => (
-          <ListItem sx={{ display: 'list-item', listStyleType: 'disc', pl: 0, ml: 2 }} key={i}>
-            <Typography variant="body2" textAlign="justify">
-              {text}
-            </Typography>
-          </ListItem>
-        ))}
-      </List>
-      <Box pb={3}>
-        <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
-          <Suspense
-            fallback={
-              <Skeleton variant="rounded">
-                <Button>
-                  <Trans>Go To Setup</Trans>
-                </Button>
-              </Skeleton>
-            }
-          >
-            <SetupCloudButton />
-          </Suspense>
-        </ErrorBoundary>
+        <TenantId />
       </Box>
     </>
   )
