c7f7967c018a8c9d9ade48d30632243deb58ed37
Sina Javaheri <sinajavaheri@gmail.com>
2023-10-16 20:01:07 +0200

    fix(get-workspace-inventory-report-summary): fix type

--- src/shared/types/server/responses/GetWorkspaceInventoryReportSummary.ts
+++ src/shared/types/server/responses/GetWorkspaceInventoryReportSummary.ts
@@ -2,6 +2,13 @@ export interface Account {
   id: string
   name: string
   cloud: string
+  score: number
+}
+
+export type FailedChecksTypeKeys = 'critical' | 'high' | 'medium' | 'low'
+
+export type FailedChecksType<value = number> = {
+  [key in FailedChecksTypeKeys]: value
 }
 
 export interface Benchmark {
@@ -11,15 +18,32 @@ export interface Benchmark {
   version: string
   clouds: string[]
   description: string
-  nr_of_checks: 0
-  failed_checks: {
+  nr_of_checks: number
+  account_summary: {
     [key in string]: {
-      [key in string]: number
+      score: number
+      failed_checks: Partial<FailedChecksType>
     }
   }
 }
 
+export interface ChangedSitatuation {
+  since: string
+  resource_count_by_kind_selection: {
+    [key in string]: number
+  }
+  resource_count_by_severity: Partial<FailedChecksType>
+}
+
 export interface GetWorkspaceInventoryReportSummaryResponse {
+  overall_score: number
+  check_summary: {
+    available_checks: number
+    failed_checks: number
+    failed_checks_by_severity: Partial<FailedChecksType>
+  }
   accounts: Account[]
   benchmarks: Benchmark[]
+  changed_vulnerable: ChangedSitatuation
+  changed_compliant: ChangedSitatuation
 }
--- src/shared/types/server/responses/index.ts
+++ src/shared/types/server/responses/index.ts
@@ -2,6 +2,12 @@ export type { GetCurrentUserResponse } from './GetCurrentUser'
 export type { GetExternalIdResponse } from './GetExternalIdResponse'
 export type { GetWorkspaceCfTemplateResponse } from './GetWorkspaceCfTemplate'
 export type { GetWorkspaceCfUrlResponse } from './GetWorkspaceCfUrl'
-export type { GetWorkspaceInventoryReportSummaryResponse } from './GetWorkspaceInventoryReportSummary'
+export type {
+  GetWorkspaceInventoryReportSummaryResponse,
+  Account as WorkspaceAccount,
+  Benchmark as WorkspaceBenchmark,
+  ChangedSitatuation as WorkspaceChangedSitatuation,
+  FailedChecksTypeKeys as WorkspaceFailedChecksTypeKeys,
+} from './GetWorkspaceInventoryReportSummary'
 export type { GetWorkspaceResponse, GetWorkspacesResponse } from './GetWorkspaces'
 export type { OAuthProviderNames, OAuthProviderResponse, OAuthProvidersResponse } from './OAuthProviders'
