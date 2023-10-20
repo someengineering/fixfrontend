8acb7e1f6f1153150f8e92aae701d848f3028620
Sina <sinajavaheri@gmail.com>
2023-10-20 12:15:59 +0200

    fix(github-build): fix the build for lack of .env file (#17)

--- src/shared/constants/endPoints.ts
+++ src/shared/constants/endPoints.ts
@@ -1,27 +1,27 @@
 export const endPoints = {
   auth: {
     jwt: {
-      logout: '/api/auth/jwt/logout',
-      login: '/api/auth/jwt/login',
+      logout: 'api/auth/jwt/logout',
+      login: 'api/auth/jwt/login',
     },
-    register: '/api/auth/register',
+    register: 'api/auth/register',
     forgotPassword: 'api/auth/forgot-password',
     resetPassword: 'api/auth/reset-password',
     requestVerifyToken: 'api/auth/request-verify-token',
     verify: 'api/auth/verify',
-    oauthProviders: '/api/auth/oauth-providers',
+    oauthProviders: 'api/auth/oauth-providers',
   },
   users: {
-    me: '/api/users/me',
+    me: 'api/users/me',
   },
   workspaces: {
-    get: '/api/workspaces/',
-    cfUrl: (workspaceId: string) => `/api/workspaces/${workspaceId}/cf_url`,
-    cfTemplate: (workspaceId: string) => `/api/workspaces/${workspaceId}/cf_template`,
-    externalId: (workspaceId: string) => `/api/workspaces/${workspaceId}/external_id`,
+    get: 'api/workspaces/',
+    cfUrl: (workspaceId: string) => `api/workspaces/${workspaceId}/cf_url`,
+    cfTemplate: (workspaceId: string) => `api/workspaces/${workspaceId}/cf_template`,
+    externalId: (workspaceId: string) => `api/workspaces/${workspaceId}/external_id`,
     inventory: {
-      reportSummary: (workspaceId: string) => `/api/workspaces/${workspaceId}/inventory/report-summary`,
+      reportSummary: (workspaceId: string) => `api/workspaces/${workspaceId}/inventory/report-summary`,
     },
-    events: (workspaceId: string) => `/api/workspaces/${workspaceId}/events`,
+    events: (workspaceId: string) => `api/workspaces/${workspaceId}/events`,
   },
 }
--- src/shared/constants/env.ts
+++ src/shared/constants/env.ts
@@ -6,9 +6,15 @@ const envToNumber = (value?: string) => {
   return returnNumber
 }
 
+const defaultOrigin = window.location.origin
+const wsOrigin = defaultOrigin.replace('http', 'ws')
+
 export const env = {
-  apiUrl: process.env.REACT_APP_USE_PROXY === 'true' ? window.location.origin : process.env.REACT_APP_SERVER,
-  wsUrl: process.env.REACT_APP_USE_PROXY === 'true' ? window.location.origin.replace('http', 'ws') : process.env.REACT_APP_WS_SERVER,
+  apiUrl: process.env.REACT_APP_USE_PROXY === 'true' ? defaultOrigin : process.env.REACT_APP_SERVER ?? defaultOrigin,
+  wsUrl:
+    process.env.REACT_APP_USE_PROXY === 'true'
+      ? wsOrigin
+      : process.env.REACT_APP_WS_SERVER ?? process.env.REACT_APP_SERVER?.replace('http', 'ws') ?? wsOrigin,
   retryCount: envToNumber(process.env.REACT_APP_NETWORK_RETRY_COUNT) ?? 5,
   webSocketRetryTimeout: envToNumber(process.env.REACT_APP_WEBSOCKET_RETRY_TIMEOUT) ?? 5_000,
 }
