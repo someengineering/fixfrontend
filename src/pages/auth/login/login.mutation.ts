18bac6731cee33f3d6dc41bc1feb98f80237fea9
Sina Javaheri <sinajavaheri@gmail.com>
2023-10-23 15:42:41 +0200

    fix(auth): fix sending username and password as encoded uri

--- src/pages/auth/login/login.mutation.ts
+++ src/pages/auth/login/login.mutation.ts
@@ -3,7 +3,7 @@ import { simpleAxios } from 'src/shared/utils/axios'
 
 export const loginMutation = async ({ username, password }: { username: string; password: string }) => {
   return simpleAxios
-    .post<string>(endPoints.auth.jwt.login, `username=${username}&password=${password}`, {
+    .post<string>(endPoints.auth.jwt.login, `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
