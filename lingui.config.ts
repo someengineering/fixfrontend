ccc14a7cbc5ee5ddceb8ba44f59925f878392268
Sina Javaheri <sinajavaheri@gmail.com>
2023-10-26 09:56:49 +0200

    fix(lingui-config): fix config to work better with ts and vite

--- lingui.config.ts
+++ lingui.config.ts
@@ -1,6 +1,8 @@
-module.exports = {
+import type { LinguiConfig } from '@lingui/conf'
+
+const config: LinguiConfig = {
   locales: ['en_US', 'de_DE'],
-  sourceLocale: 'en',
+  sourceLocale: 'en_US',
   catalogs: [
     {
       path: '<rootDir>/src/locales/{locale}/messages',
@@ -10,3 +12,5 @@ module.exports = {
   compileNamespace: 'ts',
   format: 'po',
 }
+
+export default config
