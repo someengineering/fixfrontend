b1ed1dc46a6f91c34fe8347a66f39a4dfc4aa550
Sina Javaheri <sinajavaheri@gmail.com>
2023-10-26 10:55:42 +0200

    fix(translations): update translations

--- src/shared/constants/consts.ts
+++ src/shared/constants/consts.ts
@@ -3,4 +3,4 @@ import { WorkspaceFailedChecksTypeKeys } from 'src/shared/types/server'
 
 export const sortedSeverities: WorkspaceFailedChecksTypeKeys[] = ['critical', 'high', 'medium', 'low']
 export const panelMessages = () =>
-  [{ message: 'aws-marketplace-subscribed', text: t`AWS Marketplace has been succesfully subscribed`, type: 'success' }] as const
+  [{ message: 'aws-marketplace-subscribed', text: t`AWS Marketplace has been successfully subscribed`, type: 'success' }] as const
