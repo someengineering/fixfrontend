6161d6124a083c8343948b9b2772aff79463d5c6
Sina Javaheri <sinajavaheri@gmail.com>
2023-11-20 08:37:16 +0100

    fix(error): fix generic error boundary message

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,8 +1,9 @@
 import AwsLogo from './aws-logo.svg?react'
+import DiscrodIcon from './discord-icon.svg?react'
 import FixLogo from './fix-logo.svg?react'
 import GcpLogo from './gcp-logo.svg?react'
 import GithubSEBIcon from './github-seb-icon.svg?react'
 import GoogleSEBIcon from './google-seb-icon.svg?react'
 import LogoWhiteNoBackground from './logo-white-no-background.svg?react'
 
-export { AwsLogo, FixLogo, GcpLogo, GithubSEBIcon, GoogleSEBIcon, LogoWhiteNoBackground }
+export { AwsLogo, DiscrodIcon, FixLogo, GcpLogo, GithubSEBIcon, GoogleSEBIcon, LogoWhiteNoBackground }
