7a7dd74504fc280ef30304f1bda293ebdab34c52
Sina Javaheri <sinajavaheri@gmail.com>
2023-11-29 17:32:59 +0100

    fix(lint): fix linter

--- src/shared/types/global/react-env.d.ts
+++ src/shared/types/global/react-env.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="vite/client" />
-
 import { Jsx } from 'hast-util-to-jsx-runtime'
 import { ReactElement } from 'react'
 import { Entries } from 'type-fest/source/entries.d'
@@ -14,17 +12,4 @@ declare global {
   interface ObjectConstructor {
     entries<T extends object>(o: T): Entries<T>
   }
-}
-
-interface ImportMetaEnv {
-  readonly VITE_SERVER?: string
-  readonly VITE_WS_SERVER?: string
-  readonly VITE_USE_PROXY?: string
-  readonly VITE_NETWORK_RETRY_COUNT?: string
-  readonly VITE_WEBSOCKET_RETRY_TIMEOUT?: string
-  readonly VITE_DISCORD_URL?: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
 }
