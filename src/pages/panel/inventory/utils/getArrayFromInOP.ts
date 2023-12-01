03e3a57aa6c0fccc4b02392da7048de194af5f2d
Sina Javaheri <sinajavaheri@gmail.com>
2023-12-01 11:36:52 +0100

    fix(inventory-advance-search): add null as a value

--- src/pages/panel/inventory/utils/getArrayFromInOP.ts
+++ src/pages/panel/inventory/utils/getArrayFromInOP.ts
@@ -1,6 +1,7 @@
-export const getArrayFromInOP = (value: string) =>
-  value
+export function getArrayFromInOP<WithNull extends boolean = false>(value: string, withNull?: WithNull) {
+  return value
     .substring(1, value.length - 1)
     .split(',')
-    .map((i) => i.trim())
-    .filter((i) => i)
+    .map((i) => (i === 'null' && withNull ? null : i))
+    .filter((i) => i || (withNull && i === null)) as WithNull extends true ? (string | null)[] : string[]
+}
