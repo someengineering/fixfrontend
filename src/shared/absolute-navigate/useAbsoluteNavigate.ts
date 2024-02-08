91d1e7ddf626f34c701c996b7153b8b18a499585
Sina Javaheri <sinajavaheri@gmail.com>
2024-02-08 11:49:14 +0100

    fix(error-boundary): fix used error boundary outside of use navigate

--- src/shared/absolute-navigate/useAbsoluteNavigate.ts
+++ src/shared/absolute-navigate/useAbsoluteNavigate.ts
@@ -2,9 +2,14 @@ import { useContext } from 'react'
 import { NavigateFunction } from 'react-router-dom'
 import { AbsoluteNavigateContext } from './AbsoluteNavigateContext'
 
-export function useAbsoluteNavigate(): NavigateFunction {
+export function useAbsoluteNavigate<Force extends boolean = false>(
+  force?: Force,
+): Force extends true ? NavigateFunction | undefined : NavigateFunction {
   const context = useContext(AbsoluteNavigateContext)
   if (!context) {
+    if (force) {
+      return undefined as Force extends true ? undefined : NavigateFunction
+    }
     throw new Error('useAbsoluteNavigate must be used inside the AbsoluteNavigateProvider')
   }
   return context
