aec6cf179e87e20a1d9f43cb62c1c44e2790cd8f
Lukas Lösche <lukas@some.engineering>
2024-02-12 18:09:01 +0100

    New pricing model (#99)
    * Move $ in front of price
    
    * Rename product tiers
    
    * Add new pricing information

--- src/pages/panel/workspace-settings-billing/ChangePaymentMethod.tsx
+++ src/pages/panel/workspace-settings-billing/ChangePaymentMethod.tsx
@@ -35,7 +35,7 @@ interface ChangePaymentMethodProps {
   paymentMethods: PaymentMethod[]
 }
 
-const allSecurityTiers: SecurityTier[] = ['free', 'foundational', 'high_security']
+const allSecurityTiers: SecurityTier[] = ['free', 'plus', 'business']
 
 const SecurityTierComp = ({ securityTier }: { securityTier: SecurityTier }) => {
   const label = securityTierToLabel(securityTier)
@@ -56,7 +56,7 @@ const SecurityTierComp = ({ securityTier }: { securityTier: SecurityTier }) => {
         {desc.targetCustomer}
       </Typography>
       <Stack direction="row" my={1.5} spacing={desc.oneTime ? 1.25 : 0.75} alignItems="baseline">
-        <Typography variant="h2">{desc.price.toString()}$</Typography>
+        <Typography variant="h2">${desc.price.toString()}</Typography>
         {priceDesc ? (
           <Typography variant="body2" fontWeight={600}>
             {priceDesc}*
