46118d93a68fa969d50f57b0dab405527113cb67
Lukas Lösche <lukas@some.engineering>
2024-02-22 18:35:02 +0100

    Add Beta Logo

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,6 +3,7 @@ import Crown from './crown.svg?react'
 import DiscordIcon from './discord-icon.svg?react'
 import DiscordWithTextLogo from './discord-with-text-logo.svg?react'
 import FixLogo from './fix-logo.svg?react'
+import FixBetaLogo from './fix-beta-logo.svg?react'
 import GcpLogo from './gcp-logo.svg?react'
 import GithubSEBIcon from './github-seb-icon.svg?react'
 import GoogleSEBIcon from './google-seb-icon.svg?react'
@@ -17,6 +18,7 @@ export {
   DiscordIcon,
   DiscordWithTextLogo,
   FixLogo,
+  FixBetaLogo,
   GcpLogo,
   GithubSEBIcon,
   GoogleSEBIcon,
