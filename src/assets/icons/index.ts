d8d4af6147400a06d7295d9baee969ba36363173
Lukas Lösche <lukas@some.engineering>
2024-02-22 18:44:10 +0100

    linter

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,8 +2,8 @@ import AwsLogo from './aws-logo.svg?react'
 import Crown from './crown.svg?react'
 import DiscordIcon from './discord-icon.svg?react'
 import DiscordWithTextLogo from './discord-with-text-logo.svg?react'
-import FixLogo from './fix-logo.svg?react'
 import FixBetaLogo from './fix-beta-logo.svg?react'
+import FixLogo from './fix-logo.svg?react'
 import GcpLogo from './gcp-logo.svg?react'
 import GithubSEBIcon from './github-seb-icon.svg?react'
 import GoogleSEBIcon from './google-seb-icon.svg?react'
@@ -17,8 +17,8 @@ export {
   Crown,
   DiscordIcon,
   DiscordWithTextLogo,
-  FixLogo,
   FixBetaLogo,
+  FixLogo,
   GcpLogo,
   GithubSEBIcon,
   GoogleSEBIcon,
