dd61560c3447401173e33f48e8ed8d304ecf8220
Sina Javaheri <sinajavaheri@gmail.com>
2024-02-27 16:26:47 +0100

    fix(yaml-highlighter): fix types of jsx-runtime

--- src/shared/yaml-highlighter/YamlHighlighter.tsx
+++ src/shared/yaml-highlighter/YamlHighlighter.tsx
@@ -3,6 +3,7 @@ import yaml from 'highlight.js/lib/languages/yaml'
 import highlightGithubDark from 'highlight.js/styles/atom-one-dark-reasonable.css?inline'
 import highlightGithub from 'highlight.js/styles/atom-one-light.css?inline'
 import { createLowlight } from 'lowlight'
+import { ReactElement } from 'react'
 import { Fragment, jsx, jsxs } from 'react/jsx-runtime'
 import { useThemeMode } from 'src/core/theme'
 
@@ -14,7 +15,11 @@ export const YamlHighlighter = ({ children }: { children: string }) => {
   return (
     <>
       <style>{mode === 'dark' ? highlightGithubDark : highlightGithub}</style>
-      {toJsxRuntime(tree, { Fragment, jsx, jsxs })}
+      {toJsxRuntime(tree, {
+        Fragment,
+        jsx: jsx as (type: unknown, props: unknown) => ReactElement<unknown, string | React.JSXElementConstructor<unknown>>,
+        jsxs: jsxs as (type: unknown, props: unknown) => ReactElement<unknown, string | React.JSXElementConstructor<unknown>>,
+      })}
     </>
   )
 }
