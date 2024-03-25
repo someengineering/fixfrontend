1a6865a8b3a3c2c7f3fdbdc9660b090f92fceaf0
Lukas Lösche <lukas@some.engineering>
2024-03-25 18:32:00 +0100

    Update public bucket detection rule

--- src/pages/panel/inventory/InventoryTemplateBoxes.tsx
+++ src/pages/panel/inventory/InventoryTemplateBoxes.tsx
@@ -33,7 +33,7 @@ const templateData: TemplateData[] = [
         title: t`Public S3 Buckets`,
         description: t`Identifies S3 buckets that are publicly accessible.`,
         search:
-          'is(aws_s3_bucket) and bucket_public_access_block_configuration.{block_public_acls != true or ignore_public_acls != true or block_public_policy != true or restrict_public_buckets != true} or bucket_acl.grants[*].{permission in [READ, READ_ACP] and grantee.uri = "http://acs.amazonaws.com/groups/global/AllUsers"}',
+          'is(aws_s3_bucket) and (bucket_public_access_block_configuration != null and (bucket_public_access_block_configuration.{block_public_acls != true or ignore_public_acls != true or block_public_policy != true or restrict_public_buckets != true})) or (bucket_acl.grants[*].{permission in [READ, READ_ACP] and grantee.uri = "http://acs.amazonaws.com/groups/global/AllUsers"}) or (bucket_policy.Statement[*].{Principal = "*" and (Action in [s3:GetObject, s3:PutObject] or Action[*] in [s3:GetObject, s3:PutObject])})',
       },
       {
         title: t`Open Database Access`,
