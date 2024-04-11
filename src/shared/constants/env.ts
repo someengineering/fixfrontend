6a5e54450cb104620476031a7634719377f18784
Sina Javaheri <sinajavaheri@gmail.com>
2024-04-11 15:54:17 +0200

    fix(setup-cloud-page): replace youtube with self hosted videos

--- src/shared/constants/env.ts
+++ src/shared/constants/env.ts
@@ -21,6 +21,7 @@ const env = {
     import.meta.env.VITE_USE_PROXY === 'true' && import.meta.env.MODE !== 'test'
       ? wsOrigin
       : import.meta.env.VITE_WS_SERVER ?? import.meta.env.VITE_SERVER?.replace('http', 'ws') ?? wsOrigin,
+  videosAssetsUrl: import.meta.env.VITE_VIDEOS_ASSETS_URL ?? defaultOrigin,
   retryCount: envToNumber(import.meta.env.VITE_NETWORK_RETRY_COUNT) ?? 5,
   webSocketRetryTimeout: envToNumber(import.meta.env.VITE_WEBSOCKET_RETRY_TIMEOUT) ?? 5_000,
   discordUrl: import.meta.env.VITE_DISCORD_URL ?? '#',
--- src/shared/types/global/vite-client.d.ts
+++ src/shared/types/global/vite-client.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_FIX_LANDING_PAGE_URL?: string
   readonly VITE_SERVER?: string
   readonly VITE_WS_SERVER?: string
+  readonly VITE_VIDEOS_ASSETS_URL?: string
   readonly VITE_USE_PROXY?: string
   readonly VITE_NETWORK_RETRY_COUNT?: string
   readonly VITE_WEBSOCKET_RETRY_TIMEOUT?: string
