bc3a12c087e8445e763822d7cbf4b9a1839e4d22
Lukas Lösche <lukas@some.engineering>
2024-04-22 12:23:28 +0200

    Re-order the example searches

--- src/pages/panel/inventory/InventoryTemplateBoxes.tsx
+++ src/pages/panel/inventory/InventoryTemplateBoxes.tsx
@@ -25,25 +25,25 @@ const templateData: TemplateData[] = [
     icon: SecurityIcon,
     content: [
       {
-        title: t`Unencrypted EBS Volumes`,
-        description: t`Identifies EBS volumes that are not encrypted.`,
-        search: 'is(aws_ec2_volume) and volume_encrypted = false',
-      },
-      {
         title: t`Public S3 Buckets`,
         description: t`Identifies S3 buckets that are publicly accessible.`,
         search:
           'is(aws_s3_bucket) {account_setting: <-[0:]- is(aws_account) --> is(aws_s3_account_settings)} ((account_setting.reported.bucket_public_access_block_configuration.{block_public_acls != true or ignore_public_acls != true or block_public_policy != true or restrict_public_buckets != true} and bucket_public_access_block_configuration.{block_public_acls != true or ignore_public_acls != true or block_public_policy != true or restrict_public_buckets != true}) and ((bucket_acl.grants[*].{permission in [READ, READ_ACP, WRITE, WRITE_ACP, FULL_CONTROL] and grantee.uri = "http://acs.amazonaws.com/groups/global/AllUsers"}) or (bucket_policy.Statement[*].{Effect = Allow and (Principal = "*" or Principal.AWS = "*" or Principal.CanonicalUser = "*") and (Action in ["s3:GetObject", "s3:PutObject", "s3:Get*", "s3:Put*", "s3:*", "*"] or Action[*] in ["s3:GetObject", "s3:PutObject", "s3:Get*", "s3:Put*", "s3:*", "*"])})))',
       },
       {
-        title: t`Open Database Access`,
-        description: t`Identifies databases publicly accessible from any IP on the Internet.`,
-        search: 'is(aws_rds_instance) and db_publicly_accessible = true',
+        title: t`Unencrypted EBS Volumes`,
+        description: t`Identifies EBS volumes that are not encrypted.`,
+        search: 'is(aws_ec2_volume) and volume_encrypted = false',
       },
       {
         title: t`Unencrypted Database Storage`,
         description: t`Finds databases that aren't encrypted at rest.`,
         search: 'is(aws_rds_instance) and volume_encrypted = false',
+      },
+      {
+        title: t`Open Database Access`,
+        description: t`Identifies databases publicly accessible from any IP on the Internet.`,
+        search: 'is(aws_rds_instance) and db_publicly_accessible = true',
       },
       {
         title: t`Public RDS Snapshots`,
@@ -57,9 +57,9 @@ const templateData: TemplateData[] = [
     icon: BrokenImageIcon,
     content: [
       {
-        title: t`Resources with the string "delete"`,
-        description: t`Engineers like to tag their temporary resources with the string "delete". This performs a full-text search in names, tags and other string properties.`,
-        search: '"delete"',
+        title: t`Load Balancers with no backends`,
+        description: t`Finds load balancers that are not associated with any running instances.`,
+        search: 'is(load_balancer) and backends = []',
       },
       {
         title: t`Orphaned Volumes`,
@@ -67,9 +67,10 @@ const templateData: TemplateData[] = [
         search: 'is(aws_ec2_volume) and volume_status = available and last_access > 7d',
       },
       {
-        title: t`Load Balancers with no backends`,
-        description: t`Finds load balancers that are not associated with any running instances.`,
-        search: 'is(load_balancer) and backends = []',
+        title: t`Orphaned Snapshots`,
+        description: t`Finds snapshots of disks that are no longer in use, which potentially could be deleted to reduce storage costs.`,
+        search:
+          'is(aws_ec2_snapshot,aws_rds_snapshot,aws_rds_cluster_snapshot) with(empty, <-- is(aws_ec2_volume,aws_rds_instance,aws_rds_cluster))',
       },
       {
         title: t`Unattached Elastic IPs`,
@@ -77,10 +78,9 @@ const templateData: TemplateData[] = [
         search: 'is(aws_ec2_elastic_ip) with(empty, <-- is(aws_ec2_network_interface))',
       },
       {
-        title: t`Orphaned Snapshots`,
-        description: t`Finds snapshots of disks that are no longer in use, which potentially could be deleted to reduce storage costs.`,
-        search:
-          'is(aws_ec2_snapshot,aws_rds_snapshot,aws_rds_cluster_snapshot) with(empty, <-- is(aws_ec2_volume,aws_rds_instance,aws_rds_cluster))',
+        title: t`Resources with the string "delete"`,
+        description: t`Engineers like to tag their temporary resources with the string "delete". This performs a full-text search in names, tags and other string properties.`,
+        search: '"delete"',
       },
     ],
   },
