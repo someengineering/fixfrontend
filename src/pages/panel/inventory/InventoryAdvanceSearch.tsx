860abab86ee63bc89c1c224e34c43a931582dc72
Sina <sinajavaheri@gmail.com>
2024-05-13 17:03:11 +0200

    [fix] Add an alert that something extra is in the query than forms (#208)

--- src/pages/panel/inventory/InventoryAdvanceSearch.tsx
+++ src/pages/panel/inventory/InventoryAdvanceSearch.tsx
@@ -1,10 +1,24 @@
 import { Trans } from '@lingui/macro'
 import CloseIcon from '@mui/icons-material/Close'
+import InfoIcon from '@mui/icons-material/Info'
 import ListAltIcon from '@mui/icons-material/ListAlt'
 import ManageSearchIcon from '@mui/icons-material/ManageSearch'
 import SearchIcon from '@mui/icons-material/Search'
 import { TabContext, TabList, TabPanel } from '@mui/lab'
-import { Box, Collapse, FormHelperText, IconButton, Stack, Tab, TextField, Typography } from '@mui/material'
+import {
+  Alert,
+  AlertTitle,
+  Badge,
+  Box,
+  Collapse,
+  FormHelperText,
+  IconButton,
+  Stack,
+  Tab,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@mui/material'
 import { ChangeEvent, Suspense, useCallback, useEffect, useRef, useState } from 'react'
 import { panelUI } from 'src/shared/constants'
 import { ErrorBoundaryFallback, NetworkErrorBoundary } from 'src/shared/error-boundary-fallback'
@@ -29,9 +43,11 @@ export const InventoryAdvanceSearch = ({ value: searchCrit, onChange, hasError, 
       current: { updateQuery, reset },
     },
     error,
+    uiSimpleQuery,
     q,
   } = useFixQueryParser()
   const [searchCritValue, setSearchCritValue] = useState(!searchCrit && searchCrit !== 'all' ? '' : searchCrit)
+  const hasSomethingExtra = !!q && q !== 'all' && !uiSimpleQuery()
 
   const timeoutRef = useRef<number>()
 
@@ -96,16 +112,52 @@ export const InventoryAdvanceSearch = ({ value: searchCrit, onChange, hasError, 
     <Box mb={2}>
       <TabContext value={error ? 'advance' : tab}>
         <Stack alignItems={{ xs: 'center', sm: 'end' }} borderColor="divider" borderBottom={1}>
-          <TabList onChange={(_, val) => setTab(val as TabsType)} sx={{ minHeight: 0 }}>
+          <TabList onChange={(_, val) => setTab(val as TabsType)} sx={{ minHeight: 'initial' }}>
             <Tab
               disabled={!!error}
               icon={<ListAltIcon />}
               iconPosition="start"
-              label={<Trans>Form</Trans>}
+              label={
+                hasSomethingExtra ? (
+                  <Badge
+                    slotProps={{
+                      badge: {
+                        style: { left: -30 },
+                      },
+                    }}
+                    anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+                    badgeContent={
+                      <Tooltip
+                        title={
+                          <Alert color="info" variant="outlined">
+                            <Trans>
+                              <AlertTitle>This search is complex.</AlertTitle>
+                              The combo boxes below may not display all filter details but can help narrow your results. For full control,
+                              please use the Advanced tab.
+                            </Trans>
+                          </Alert>
+                        }
+                      >
+                        <InfoIcon color="info" />
+                      </Tooltip>
+                    }
+                  >
+                    <Trans>Form</Trans>
+                  </Badge>
+                ) : (
+                  <Trans>Form</Trans>
+                )
+              }
               value="form"
-              sx={{ minHeight: 0 }}
+              sx={{ minHeight: 'initial' }}
             />
-            <Tab icon={<ManageSearchIcon />} iconPosition="start" label={<Trans>Advanced</Trans>} value="advance" sx={{ minHeight: 0 }} />
+            <Tab
+              icon={<ManageSearchIcon />}
+              iconPosition="start"
+              label={<Trans>Advanced</Trans>}
+              value="advance"
+              sx={{ minHeight: 'initial' }}
+            />
           </TabList>
         </Stack>
         <TabPanel value="form" sx={{ p: 0 }}>
