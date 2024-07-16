babc86aafaf485091743aa359a1998d116783fd1
Sina <sinajavaheri@gmail.com>
2024-07-16 09:00:10 +0200

    [fix] update error message on product tier change (#272)

--- src/pages/panel/workspace-settings-billing/putWorkspaceBilling.mutation.ts
+++ src/pages/panel/workspace-settings-billing/putWorkspaceBilling.mutation.ts
@@ -1,9 +1,9 @@
 import { endPoints } from 'src/shared/constants'
-import { GetWorkspaceSettingsResponse, PutWorkspaceBillingRequest } from 'src/shared/types/server'
+import { GetWorkspaceBillingResponse, PutWorkspaceBillingRequest } from 'src/shared/types/server'
 import { axiosWithAuth } from 'src/shared/utils/axios'
 
 export const putWorkspaceBillingMutation = async ({ workspaceId, ...body }: PutWorkspaceBillingRequest & { workspaceId: string }) => {
   return workspaceId
-    ? axiosWithAuth.put<GetWorkspaceSettingsResponse>(endPoints.workspaces.workspace(workspaceId).billing, body).then((res) => res.data)
-    : ({} as GetWorkspaceSettingsResponse)
+    ? axiosWithAuth.put<GetWorkspaceBillingResponse>(endPoints.workspaces.workspace(workspaceId).billing, body).then((res) => res.data)
+    : ({} as GetWorkspaceBillingResponse)
 }
