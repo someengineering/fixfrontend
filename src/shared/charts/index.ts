a97d537b49d4584c7a093f35a29c5c976778b6a9
Sina Javaheri <sinajavaheri@gmail.com>
2024-07-29 18:07:08 +0200

    fix(charts): re-optimize sorts

--- src/shared/charts/index.ts
+++ src/shared/charts/index.ts
@@ -1,5 +1,5 @@
+export { groupToColor, groupToColorMaterial } from './groupUtils'
 export { Heatmap } from './Heatmap'
 export { NetworkDiagram } from './NetworkDiagram'
 export { PieChart } from './PieChart'
 export { StackbarChart } from './StackbarChart'
-export { groupToColor, groupToColorMaterial } from './groupUtils'
