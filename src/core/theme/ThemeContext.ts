55ad90568aca018e0d84fcda52cf62ae4ed8f728
Sina Javaheri (Sijav) <sinajavaheri@gmail.com>
2024-11-05 11:42:40 +0100

    fix(theme-context): fix wrong type

--- src/core/theme/ThemeContext.ts
+++ src/core/theme/ThemeContext.ts
@@ -5,7 +5,7 @@ export type ThemeContextRealValues = {
 }
 
 export interface ThemeContextValue extends Partial<ThemeContextRealValues> {
-  toggleColorMode: (mode: ThemeContextRealValues['mode']) => void
+  toggleColorMode: (mode?: ThemeContextRealValues['mode']) => void
 }
 
 export const ThemeContext = createContext<ThemeContextValue | null>(null)
